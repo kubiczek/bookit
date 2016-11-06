3df927df86799e3cd873a66e9d6aa51f260f479c
Marcin Kubiczek <marcin.kubiczek@barklayscorp.com>
2016-11-06 23:18:58 +0000

    Create BookitCalendar from the batch and convert it to String

--- src/main/java/com/kubiczek/bookit/BookitCalendar.java
+++ src/main/java/com/kubiczek/bookit/BookitCalendar.java
@@ -1,18 +1,24 @@
 package com.kubiczek.bookit;
 
+import com.kubiczek.bookit.model.BookingBatch;
 import com.kubiczek.bookit.model.BookingRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
 /**
  * Created by mkubiczek on 11/6/2016.
  */
 public class BookitCalendar {
+    private static final Logger log = LoggerFactory.getLogger(BookitCalendar.class);
+
     private final LocalTime officeHoursStart, officeHoursEnd;
     private Map<LocalDate, Set<BookingRequest>> calendar = new HashMap<>();
 
@@ -20,6 +26,21 @@ public class BookitCalendar {
         assert officeHoursStart.isBefore(officeHoursEnd);
         this.officeHoursStart = officeHoursStart;
         this.officeHoursEnd = officeHoursEnd;
+    }
+
+    public BookitCalendar(BookingBatch batch) {
+        this(batch.getOfficeHoursStart(), batch.getOfficeHoursEnd());
+        List<BookingRequest> bookings = batch.getBookings();
+        Collections.sort(bookings,
+                (o1, o2) -> o1.getSubmissionTime().compareTo(o2.getSubmissionTime()));
+        bookings.forEach(request -> {
+            try {
+                this.add(request);
+            } catch (BookitException e) {
+                log.warn("Request ignored: {}", e.getMessage());
+            }
+        });
+
     }
 
     public void add(BookingRequest request) {
@@ -52,5 +73,26 @@ public class BookitCalendar {
 
     public LocalTime getOfficeHoursEnd() {
         return officeHoursEnd;
+    }
+
+    @Override
+    public String toString() {
+        List<LocalDate> days = calendar.keySet().stream().collect(toList());
+        Collections.sort(days);
+
+        return days.stream().map(day -> {
+                    StringBuffer result = new StringBuffer();
+                    result.append(day.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
+                            .append('\n')
+                            .append(calendar.get(day).stream()
+                                    .map(request -> "" +
+                                            request.getMeetingStartTime().toLocalTime().format(DateTimeFormatter.ofPattern("HH:mm")) + " " +
+                                            request.getMeetingEndTime().toLocalTime().format(DateTimeFormatter.ofPattern("HH:mm")) + " " +
+                                            request.getEmployeeId()
+                                    )
+                                    .collect(joining("\n")));
+                    return result.toString();
+                }
+        ).collect(joining("\n"));
     }
 }
--- src/test/java/com/kubiczek/bookit/BookitCalendarTest.java
+++ src/test/java/com/kubiczek/bookit/BookitCalendarTest.java
@@ -1,9 +1,15 @@
 package com.kubiczek.bookit;
 
+import com.kubiczek.bookit.model.BookingBatch;
 import com.kubiczek.bookit.model.BookingRequest;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.StringReader;
+import java.net.URISyntaxException;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -32,6 +38,47 @@ public class BookitCalendarTest {
         // then
         assertThat(calendar.get(LocalDate.of(2016, 11, 6))).hasSize(1);
         assertThat(calendar.get(LocalDate.of(2016, 11, 6))).contains(booking);
+        assertThat(calendar.toString()).isEqualTo("" +
+                "2016-11-06\n" +
+                "15:30 17:30 EMP001");
+    }
+
+    @Test
+    public void shouldCreateCalendar_whenBatchIsProvidedAsString() {
+        // given
+        BookingBatch batch = BookingBatchFactory.newInstance(new StringReader("" +
+                "0900 1730" + '\n' +
+                "2015-08-17 10:17:06 EMP001 2015-08-21 09:00 2" + '\n' +
+                "2015-08-16 12:34:56 EMP002 2015-08-21 09:00 2" + '\n' +
+                "2015-08-16 09:28:23 EMP003 2015-08-22 14:00 2" + '\n' +
+                "2015-08-17 11:23:45 EMP004 2015-08-22 16:00 1" + '\n' +
+                "2015-08-15 17:29:12 EMP005 2015-08-21 16:00 3"
+        ));
+        // when
+        calendar = new BookitCalendar(batch);
+        // then
+        assertThat(calendar.toString()).isEqualTo("" +
+                "2015-08-21\n" +
+                "09:00 11:00 EMP002\n" +
+                "2015-08-22\n" +
+                "14:00 16:00 EMP003\n" +
+                "16:00 17:00 EMP004");
+    }
+
+    @Test
+    public void shouldCreateCalendar_whenBatchIsProvidedAsFile() throws FileNotFoundException, URISyntaxException {
+        // given
+        BookingBatch batch = BookingBatchFactory.newInstance(new FileReader(
+               getClass().getClassLoader().getResource("booking-requests.bookit").getFile()));
+        // when
+        calendar = new BookitCalendar(batch);
+        // then
+        assertThat(calendar.toString()).isEqualTo("" +
+                "2015-08-21\n" +
+                "09:00 11:00 EMP002\n" +
+                "2015-08-22\n" +
+                "14:00 16:00 EMP003\n" +
+                "16:00 17:00 EMP004");
     }
 
     @Test
